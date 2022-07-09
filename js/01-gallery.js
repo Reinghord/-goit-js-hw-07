8dceaaa7a7170f191d1f2ca77b4e851e652251ea
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-09 10:56:09 +0300

    finished HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//Refs
+const { gallery } = {
+  gallery: document.body.querySelector(".gallery"),
+};
+
+//Creating Markup
+galleryItems.forEach((item) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" onclick="event.preventDefault()">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+//Adding Events
+gallery.addEventListener("click", () => {
+  const instance = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`
+  );
+  instance.show();
+
+  if (basicLightbox.visible()) {
+    document.addEventListener("keydown", () => {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    });
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//Refs
+const { gallery } = {
+  gallery: document.body.querySelector(".gallery"),
+};
+
+//Creating markup
+galleryItems.forEach((item) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="test" />
+</a>`
+  );
+});
+
+//Initialize SimpleLightBox
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
